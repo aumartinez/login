1168df05d526586e5e8f57b85a1426408d826aca
aumartinez <acm_asd@hotmail.com>
2019-03-20 12:26:51 -0400

    Create bottomscripts.php

---
+++ inc/bottomscripts.php
@@ -0,0 +1,6 @@
+<!--bottom JavaScript section-->
+<!--JQuery (necessary for Bootstrap's JavaScript plugins)-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script>
+<!--Bootstrap compiled plugin-->
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+<?php echo "\n"; ?>
