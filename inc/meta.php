be63c351c970110b7c0b19a5f9ff3a9dd8444b0a
aumartinez <acm_asd@hotmail.com>
2019-03-20 12:16:49 -0400

    Create meta.php

---
+++ inc/meta.php
@@ -0,0 +1,10 @@
+<!--Meta tags-->
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta name="robots" content="noindex, nofollow"/>
+
+<!--Stylesheets-->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+<link href="css/mycss.css" rel="stylesheet" />
