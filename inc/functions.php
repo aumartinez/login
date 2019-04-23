3185db93c4470ad178b2f8a4daf338765e244314
Augusto Martinez <acm_asd@hotmail.com>
2019-04-23 16:44:23 -0400

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -1,38 +1,8 @@
 <?php 
 //Functions
-function cutString($string, $string_limit){
-	
-	//avoid breaking html
-	$string = strip_tags($string);
-	
-	//check length of string
-	if(strlen($string) > $string_limit){
-		//truncate string
-		$stringCut = substr($string, 0, $string_limit);
-		$string = substr($stringCut, 0, strrpos($stringCut, ' ')).' ...';
-	}
-	else{
-		$string;
-	}
-	return $string;
-}
 
 function generateRandomString($length){
 	return substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"), 0, $length);
-}
-
-function getTheIp(){
-	if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-		$ip = $_SERVER['HTTP_CLIENT_IP'];
-	} 
-	elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-		$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-	} 
-	else {
-		$ip = $_SERVER['REMOTE_ADDR'];
-	}
-	
-	return $ip;
 }
 
 ?>
