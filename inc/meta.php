ec9af9ca72cfd77737557cfba855ef81a8709c64
Augusto Martinez <acm_asd@hotmail.com>
2019-04-25 09:45:45 -0400

    Update meta.php

--- inc/meta.php
+++ inc/meta.php
@@ -2,7 +2,7 @@
 <meta charset="utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<meta name="robots" content="noindex, nofollow"/>
+<meta name="robots" content="noindex, nofollow" />
 
 <!--Stylesheets-->
 <link href="css/bootstrap.min.css" rel="stylesheet" />
