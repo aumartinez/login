922e7e67c5d731bcef400bf28eb5ec24195a85e4
Augusto Martinez <acm_asd@hotmail.com>
2019-04-25 16:57:42 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,137 +1,136 @@
 <?php
-ob_start();
-//Here clear current credentials and authorization
-
-session_start();
-header('HTTP/1.0 401 Unauthorized');
-
-?>
+   ob_start();
+   //Here clear current credentials and authorization
+   
+   session_start();
+   header('HTTP/1.0 401 Unauthorized');
+   
+   ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<title>Login page</title>
-<?php include 'inc/meta.php';?>
-</head>
-<body class="signin">
-
-<div class="container">
-	<div id="body-content" class="row">
-  <div class="col-xs-8 centered">
-<?php
-
-			$username = "";
-			$userpass = "";
-			
-			isset($_POST['username'])?$username = $_POST['username']:$username = false;
-			isset($_POST['userpass'])?$userpass = trim($_POST['userpass']):$userpass = false;
-			isset($_POST['userpass'])?$originalpass = trim($_POST['userpass']):$originalpass = false;
-			isset($_POST['submit'])?$submit = $_POST['submit']:$submit=false;
-			
-			if($submit){
-				if($username && $userpass){
-					include 'inc/mysqlconnect.php';
-					include 'inc/functions.php';
-					
-					$getUser =	"SELECT *
-					FROM table_users
-					WHERE username = '$username'";
-					
-					$getUserQuery = mysqli_query($conx, $getUser);
-					
-					if(!$getUserQuery){
-					  die("Error: ".mysqli_error($conx));
-					}
-					
-					$getSalt = mysqli_fetch_assoc($getUserQuery);
-					$salt = trim($getSalt['userpass2']);	
-					
-					mysqli_free_result($getUserQuery);
-					
-					$crypted = trim(crypt($userpass, $salt));
-					
-					$userpass = $crypted;
-					
-					$matchPass =	"SELECT *
-					FROM table_users
-					WHERE username = '$username' 
-					AND userpass1 = '$userpass'";
-					
-					$matchPassQuery = mysqli_query($conx, $matchPass);
-					
-					if(!$matchPassQuery){
-					  die("Error ".mysqli_error($conx));
-					}
-					
-					$numrows = mysqli_num_rows($matchPassQuery);
-					
-					mysqli_free_result($matchPassQuery);
-															
-					if($numrows == 1){
-						$_SESSION['logged'] = true;
-						$_SESSION['username'] = $username;			
-						
-						//SHA-512 hash
-						$newsalt = '$6$rounds=5000$'.generateRandomString(8).'$';
-						$newcrypted = substr(crypt($originalpass, $newsalt),strlen($newsalt));
-						$newpass = $newcrypted;
-						
-						$sql =	"UPDATE table_users
-						SET userpass1 = '$newpass', userpass2= '$newsalt'
-						WHERE username = '$username'";
-						
-						$update = mysqli_query($conx, $sql);
-						
-						if(!$update){
-							die("Error :".mysqli_error($conx));
-						}
-												
-						mysqli_close($conx);	
-
-						header('Location:index.php');						
-						exit();
-					}		
-					
-					else{
-						$_SESSION['logged'] = false;						
-						mysqli_close($conx);
-						header_remove(); 
-					}
-				}
-				
-				$_SESSION['logged'] = false;
-				header_remove(); 
-			}
-?>
-		
-		<form id="login-form" name="login" class="form-signin" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-			
-			<h2 class="form-signin-heading">Please sign in</h2>
-			
-			<label class="sr-only">User name</label>
-			<input id="input-user" type="text" name="username" class="form-control" placeholder="User name" required autofocus autocomplete="off"/>
-			<label class="sr-only">Password</label>
-			<input id="input-pass" type="password" name="userpass" class="form-control" placeholder="Password" required autofocus />
-			<input class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="Sign in" />
-			
-		</form>
-<?php 
-			
-			if($submit){
-				if($_SESSION['logged']==false){
-					echo "<h3 class=\"red-text\" align=\"center\">";
-					echo "Try again";		
-					echo "</h3>\n";
-				}
-			}
-?>
-	</div>	
-	</div>
-</div>
-
-<?php include 'inc/bottomscripts.php'; ?>
-
-</body>
+   <head>
+      <title>Login page</title>
+      <?php include 'inc/meta.php';?>
+   </head>
+   <body>
+      <div class="container">
+         <div class="row">
+            <div class="col-xs-8 centered">
+               <?php
+                  $username = "";
+                  $userpass = "";
+                  
+                  isset($_POST['username'])?$username = $_POST['username']:$username = false;
+                  isset($_POST['userpass'])?$userpass = trim($_POST['userpass']):$userpass = false;
+                  isset($_POST['userpass'])?$originalpass = trim($_POST['userpass']):$originalpass = false;
+                  isset($_POST['submit'])?$submit = $_POST['submit']:$submit=false;
+                  
+                  if($submit){
+                  	if($username && $userpass){
+                  		include 'inc/mysqlconnect.php';
+                  		include 'inc/functions.php';
+                  		
+                  		$getUser =	"SELECT *
+                  		FROM table_users
+                  		WHERE username = '$username'";
+                  		
+                  		$getUserQuery = mysqli_query($conx, $getUser);
+                  		
+                  		if(!$getUserQuery){
+                  		  die("Error: ".mysqli_error($conx));
+                  		}
+                  		
+                  		$getSalt = mysqli_fetch_assoc($getUserQuery);
+                  		$salt = trim($getSalt['userpass2']);	
+                  		
+                  		mysqli_free_result($getUserQuery);
+                  		
+                  		$crypted = trim(crypt($userpass, $salt));
+                  		
+                  		$userpass = $crypted;
+                  		
+                  		$matchPass =	"SELECT *
+                  		FROM table_users
+                  		WHERE username = '$username' 
+                  		AND userpass1 = '$userpass'";
+                  		
+                  		$matchPassQuery = mysqli_query($conx, $matchPass);
+                  		
+                  		if(!$matchPassQuery){
+                  		  die("Error ".mysqli_error($conx));
+                  		}
+                  		
+                  		$numrows = mysqli_num_rows($matchPassQuery);
+                  		
+                  		mysqli_free_result($matchPassQuery);
+                  												
+                  		if($numrows == 1){
+                  			$_SESSION['logged'] = true;
+                  			$_SESSION['username'] = $username;			
+                  			
+                  			//SHA-512 hash
+                  			$newsalt = '$6$rounds=5000$'.generateRandomString(8).'$';
+                  			$newcrypted = substr(crypt($originalpass, $newsalt),strlen($newsalt));
+                  			$newpass = $newcrypted;
+                  			
+                  			$sql =	"UPDATE table_users
+                  			SET userpass1 = '$newpass', userpass2= '$newsalt'
+                  			WHERE username = '$username'";
+                  			
+                  			$update = mysqli_query($conx, $sql);
+                  			
+                  			if(!$update){
+                  				die("Error :".mysqli_error($conx));
+                  			}
+                  									
+                  			mysqli_close($conx);	
+                  
+                  			header('Location:index.php');						
+                  			exit();
+                  		}		
+                  		
+                  		else{
+                  			$_SESSION['logged'] = false;						
+                  			mysqli_close($conx);
+                  			header_remove(); 
+                  		}
+                  	}
+                  	
+                  	$_SESSION['logged'] = false;
+                  	header_remove(); 
+                  }
+                  ?>
+               <form id="login-form" name="login" class="form-signin" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                  <h2 class="form-signin-heading">Please sign in</h2>
+                  <label class="sr-only">User name</label>
+                  <input id="input-user" type="text" name="username" class="form-control" placeholder="User name" required autofocus autocomplete="off"/>
+                  <label class="sr-only">Password</label>
+                  <input id="input-pass" type="password" name="userpass" class="form-control" placeholder="Password" required autofocus />
+                  <input class="btn btn-lg btn-primary btn-block" type="submit" name="submit" value="Sign in" />
+               </form>
+               <?php 
+                  if($submit){
+                  	if($_SESSION['logged']==false){
+                  		echo "<h3 class=\"red-text\" align=\"center\">";
+                  		echo "Try again";		
+                  		echo "</h3>\n";
+                  	}
+                  }
+                  ?>
+            </div>
+         </div>
+		 
+		 <div class="row">
+		   <div class="col-xs-8 centered">
+			 <p>Try this<br />
+			 User: guest<br />
+			 Pass: Test
+		   </div>
+		 </div>
+      </div>
+      <?php include 'inc/bottomscripts.php'; ?>
+   </body>
 </html>
 <?php
-ob_end_flush();
-?>
+   ob_end_flush();
+   ?>
