58cd51520dc8160bfe84eeb5ed387c85c352caf9
Augusto Martinez <acm_asd@hotmail.com>
2019-06-25 20:18:03 -0600

    Update meta.php

--- inc/meta.php
+++ inc/meta.php
@@ -8,3 +8,4 @@
 <link href="css/bootstrap.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 <link href="css/mycss.css" rel="stylesheet" />
+<?php echo "\n"; ?>
